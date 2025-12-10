5c3f295f71e92ba318d9612f2ebd89998c801324
Krunal022 <krunalvaghela723@gmail.com>
2025-12-10 21:00:07 +0530

    add/render tasks with complete & delete actions

--- script.js
+++ script.js
@@ -18,3 +18,66 @@ function openFeature() {
     })
 }
 openFeature()
+
+function todoFeature() {
+    let currentTask = []
+
+    if (localStorage.getItem("currentTask")) {
+        currentTask = JSON.parse(localStorage.getItem("currentTask"))
+    } else {
+        console.log("There is no Task List!")
+    }
+
+    let form = document.querySelector('.todo-form form')
+    let headingInp = document.querySelector('form input')
+    let detailInp = document.querySelector('form textarea')
+    let checkbox = document.querySelector('form .imp-check #check')
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        console.log(currentTask)
+        currentTask.push({
+            heading: headingInp.value,
+            detail: detailInp.value,
+            imp: checkbox.checked
+        })
+        renderTask()
+
+        location.reload()
+    })
+
+
+    function renderTask() {
+        let allTask = document.querySelector('.todo-container .allTask')
+        let sum = ''
+
+        currentTask.forEach((e, idx) => {
+            sum = sum + ` <div class="task">
+                        <div class="task-data">
+                            <h1>${e.heading} <span class=${e.imp}>imp</span></h1>
+                            <h3>${e.detail}</h3>
+                        </div>
+                        <div class="btn" >
+                            <button id=${idx}> Mark as Completed</button>
+                        </div>
+                    </div>`
+        })
+        allTask.innerHTML = sum
+
+        localStorage.setItem("currentTask", JSON.stringify(currentTask))
+    }
+    renderTask()
+
+
+    // Delete perticular data
+    let marksCompletedBtn = document.querySelectorAll('.task .btn button')
+
+    marksCompletedBtn.forEach(function (btn) {
+        btn.addEventListener("click", () => {
+            currentTask.splice(btn.id, 1)
+            renderTask();
+            location.reload()
+        })
+    })
+}
+todoFeature()
