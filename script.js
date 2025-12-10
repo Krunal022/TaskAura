e9cf3028a795fe42ad815d57a05e61df2900ceda
Krunal022 <krunalvaghela723@gmail.com>
2025-12-10 17:19:23 +0530

    todo interface with full responsive

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-let allElems = document.querySelectorAll('.elem')
-let fullElemPage = document.querySelectorAll(".fullElem")
-let fullElemPagebackbtn = document.querySelectorAll('.back')
+function openFeature() {
+    let allElems = document.querySelectorAll('.elem')
+    let fullElemPage = document.querySelectorAll(".fullElem")
+    let fullElemPagebackbtn = document.querySelectorAll('.back')
 
-// Opening the fullPage
-allElems.forEach(function (elem) {
-    elem.addEventListener('click', () => {
-        fullElemPage[elem.id].style.display = "block"
+    // Opening the fullPage
+    allElems.forEach(function (elem) {
+        elem.addEventListener('click', () => {
+            fullElemPage[elem.id].style.display = "block"
+        })
     })
-})
 
-// Closing the fullPage
-fullElemPagebackbtn.forEach(function (back) {
-    back.addEventListener('click', () => {
-        fullElemPage[back.id].style.display = "none"
+    // Closing the fullPage
+    fullElemPagebackbtn.forEach(function (back) {
+        back.addEventListener('click', () => {
+            fullElemPage[back.id].style.display = "none"
+        })
     })
-})
+}
+openFeature()
